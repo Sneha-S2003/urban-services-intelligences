33fb6bd2d23f7f2088be147974e184b59af88af1
Sneha-S2003 <snehasridhar@gmail.com>
2026-02-15 21:38:28 -0500

    Add Mapbox map and short trips layer
    Initialize a Mapbox map and add a GeoJSON source with a layer for short trips.

---
+++ js/short-trips.js
@@ -0,0 +1,41 @@
+mapboxgl.accessToken = 'YOUR_MAPBOX_TOKEN';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v11',
+  center: [-77.0369, 38.9072],
+  zoom: 11
+});
+
+map.on('load', () => {
+
+  map.addSource('shortTrips', {
+    type: 'geojson',
+    data: 'data/short_trip_saturation.geojson'
+  });
+
+  map.addLayer({
+    id: 'shortTrips-layer',
+    type: 'circle',
+    source: 'shortTrips',
+    paint: {
+      'circle-radius': [
+        'interpolate',
+        ['linear'],
+        ['get', 'avg_daily_short_trips'],
+        0, 3,
+        40, 12
+      ],
+      'circle-color': [
+        'interpolate',
+        ['linear'],
+        ['get', 'short_trip_share'],
+        0, '#e5e7eb',
+        0.5, '#60a5fa',
+        0.8, '#1d4ed8'
+      ],
+      'circle-opacity': 0.85
+    }
+  });
+
+});
