5072b96b2d3857c5590f42e609f672c4e8e25906
Sneha-S2003 <snehasridhar@gmail.com>
2026-02-21 11:52:21 -0500

    Change map style and remove demand layer
    Updated the map style and removed the demand layer configuration.

--- js/demand.js
+++ js/demand.js
@@ -2,33 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic3NyaWRoYXIiLCJhIjoiY21sb2prdWUxMHQ1ZjNkcHV3N
 
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/light-v11',
+  style: 'mapbox://styles/mapbox/streets-v11',
   center: [-77.0369, 38.9072],
   zoom: 11
-});
-
-map.on('load', () => {
-
-  map.addSource('demand', {
-    type: 'geojson',
-    data: 'station_stress_mismatch.geojson'
-  });
-
-  map.addLayer({
-    id: 'demand-layer',
-    type: 'circle',
-    source: 'demand',
-    paint: {
-      'circle-radius': [
-        'interpolate',
-        ['linear'],
-        ['get', 'avg_daily_trips'],
-        0, 3,
-        60, 14
-      ],
-      'circle-color': '#1f2937',
-      'circle-opacity': 0.85
-    }
-  });
-
 });
