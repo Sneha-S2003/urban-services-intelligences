90dda1e157a856e74917ae4d360e75482a567c27
Sneha-S2003 <snehasridhar@gmail.com>
2026-02-21 11:54:00 -0500

    Change map style to light and add demand layer
    Updated the map style to light and added a demand layer with a geojson source.

--- js/demand.js
+++ js/demand.js
@@ -2,7 +2,27 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic3NyaWRoYXIiLCJhIjoiY21sb2prdWUxMHQ1ZjNkcHV3N
 
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
+  style: 'mapbox://styles/mapbox/light-v11',
   center: [-77.0369, 38.9072],
   zoom: 11
+});
+
+map.on('load', () => {
+
+  map.addSource('demand', {
+    type: 'geojson',
+    data: 'station_stress_mismatch.geojson'  // <-- confirm correct file name
+  });
+
+  map.addLayer({
+    id: 'demand-layer',
+    type: 'circle',
+    source: 'demand',
+    paint: {
+      'circle-radius': 6,
+      'circle-color': '#111827',
+      'circle-opacity': 0.8
+    }
+  });
+
 });
